3fa71e85dabec42ba3235908b010920d3712828f
MPapyrus <secenmasterdead@yandex.ru>
2025-05-16 12:05:02 +0500

    Created first receiver MyReceiver.kt

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -10,12 +10,20 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private val receiver = MyReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_LOW)
+
+        registerReceiver(receiver, intentFilter)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
 
+        unregisterReceiver(receiver)
+    }
 }
